f1c06a39b286afe3c6fa03684eb182edc5a59fdf
Leegyu66 <96297784+Leegyu66@users.noreply.github.com>
2025-06-24 17:40:49 +0900

    Update session.py

--- app/db/session.py
+++ app/db/session.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URI = "mariadb+pymysql://choi:1234@mariadb:3306/board_api"
+DATABASE_URI = "mariadb+pymysql://username:password@mariadb:3306/board_api"
 
 engine = create_engine(DATABASE_URI)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
